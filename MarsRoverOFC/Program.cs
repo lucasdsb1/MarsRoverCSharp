de0bc460723305c23a4ee9f63637e934496d4094
Lucas Duarte <ldsb1@ifal.edu.br>
2022-06-13 13:33:12 -0300

    feat: multiplos rovers

--- MarsRoverOFC/Program.cs
+++ MarsRoverOFC/Program.cs
@@ -16,23 +16,35 @@ try
     var movimentoService = serviceProvider.GetService<IMovimentoService>();
     var posicaoService = serviceProvider.GetService<IPosicaoService>();
     var roverService = serviceProvider.GetService<IRoverService>();
-
-    Console.WriteLine("Coordenadas: (Formato aceito: 5 5)");
+    var roverList = new List<Rover>();
+    
+    Console.WriteLine("Coordenadas: (Formato aceito: X:int Y:int. Ex.: 5 5)");
     var plator = platorService?.ConverterParaPlator(Console.ReadLine() ?? string.Empty);
 
-    Console.WriteLine("\nPosição inicial do Rover: (Formato aceito: 1 2 N)");
-    var posicao = posicaoService?.ConverterParaPosicao(Console.ReadLine() ?? string.Empty, plator ?? throw new Exception("Plator não pode ser nulo!"));
+    do
+    {
+        Console.WriteLine("\nPosição inicial do Rover: (Formato aceito: X:int Y:int A:char. Ex.: 1 2 N)");
+        var posicao = posicaoService?.ConverterParaPosicao(Console.ReadLine() ?? string.Empty, plator ?? throw new Exception("Plator não pode ser nulo!"));
 
-    Console.WriteLine("\nMovimentos: (Formato aceito: LMLMLMLMRM)");    
-    var movimentos = movimentoService?.ConverterParaMovimento(Console.ReadLine() ?? string.Empty);
+        Console.WriteLine("\nMovimentos: (Letras aceitas: L para LEFT | R para RIGHT | M para MOVE. Ex: LMLMLMLMM)");
+        var movimentos = movimentoService?.ConverterParaMovimento(Console.ReadLine() ?? string.Empty);
 
-    var rover = new Rover(
-        plator ?? throw new Exception("Plator não pode ser nulo!"), 
-        posicao ?? throw new Exception("Posicao não pode ser nulo!"), 
-        movimentos ?? throw new Exception("Movimentos não pode ser nulo!"));
-    rover = roverService?.ExecutarMovimentos(rover);
+        var rover = new Rover(
+            plator ?? throw new Exception("Plator não pode ser nulo!"),
+            posicao ?? throw new Exception("Posicao não pode ser nulo!"),
+            movimentos ?? throw new Exception("Movimentos não pode ser nulo!"));
 
-    Console.WriteLine($"\n{rover.Posicao.X} {rover.Posicao.Y} {rover.Posicao.Direcao}");
+        roverList.Add(rover);        
+
+        Console.WriteLine("\n---------------");
+        Console.WriteLine("Deseja definir outro ROVER? (QUALQUER TECLA = SIM | N = NÃO)");
+    } while (Console.ReadLine()?.ToUpper() != "N");
+
+    foreach (var lastPosition in roverList.Select(rover => roverService?.ExecutarMovimentos(rover).Posicao))
+    {
+        Console.WriteLine($"\n{lastPosition?.X} {lastPosition?.Y} {lastPosition?.Direcao}");
+    }
+
     Console.ReadKey();
 }
 catch (Exception ex)
